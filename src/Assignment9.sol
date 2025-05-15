0dfc5b63e4673a84ed1c742a0d3a613300578b16
mohammedelgamal <47455343+mohammedelgamal@users.noreply.github.com>
2025-05-15 17:12:43 +0300

    Update Assignment9.sol
    my solution

--- src/Assignment9.sol
+++ src/Assignment9.sol
@@ -1,25 +1,25 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-// import Openzeppelin Ownable contract
+// import OpenZeppelin Ownable contract
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 // inherit from Ownable contract 
-contract Assignment9 {
-    // create a public array called `deployedContracts` to store contracts addresses in it 
+contract Assignment9 is Ownable {
+    // create a public array called `deployedContracts` to store contract addresses in it 
+    address[] public deployedContracts;
 
-    // call Ownable constructor of openzeppelin contract
-    constructor () {
-        
-    }
+    // call Ownable constructor of OpenZeppelin contract
+    constructor() Ownable(msg.sender) {}
 
     // @dev Factory to deploy new instances of `SimpleContract`
     // make only the owner is the one who can call this function
-    function createContract() external {
+    function createContract() external onlyOwner {
         // deploy new instance of SimpleContract
-            // Hint: When you create a new instance, do not forget to pass any arbitrary initial value in the constructor
+        SimpleContract newContract = new SimpleContract(123); // pass any arbitrary initial value
         
-
         // push the new deployed instance to deployedContracts array
+        deployedContracts.push(address(newContract));
     }
 }
 
